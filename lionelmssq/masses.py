0a6deb93de47bf0e90fb5c6f6bd481235d72c055
Laura C. Kühle <laura.kuehle@hhu.de>
2025-06-27 00:20:02 +0200

    Moved initialization of mass dataframes into separate function.

--- lionelmssq/masses.py
+++ lionelmssq/masses.py
@@ -10,33 +10,8 @@ REDUCE_TABLE = True
 REDUCE_SET = False
 COMPRESSION_RATE = 32
 
-MASSES = pl.read_csv(
-    (
-        importlib.resources.files(__package__)
-        / "assets"
-        / f"{"masses_bases" if REDUCE_SET else "masses"}.tsv"
-    ),
-    separator="\t",
-)
-# Note: "masses.tsv" has multiples nucleosides with the same mass!
-
-assert MASSES.columns == _COLS
-
-# Uncomment if we only want to consider the natural (unmodified) nucleosides i.e. [A,G,U,C]!
-# MASSES = MASSES.filter(pl.col("nucleoside").is_in(["A", "G", "U", "C"]))
-
 ROUND_DECIMAL = 5  # The precision (after decimal points) to which to consider the nucleoside masses.
 # In the nucleoside table, it can happen that the same masses may be reported with different precision values. So UNIQUE MASSES after rounding may not be unique without doing the above step!
-
-MASSES = MASSES.with_columns(pl.col("monoisotopic_mass").round(ROUND_DECIMAL))
-
-UNIQUE_MASSES = (
-    MASSES.group_by("monoisotopic_mass", maintain_order=True)
-    .first()
-    .select(pl.col(_COLS))
-)
-
-# For mass explanation for ladder building, to convert the masses to an integer value for the DP algorithm!
 
 TOLERANCE = 1e-3  # For perfect matching, the TOLERANCE should be the
 # precision (digits after decimal) to which the masses of nucleosides and sequences are reported, i.e. 1e-(ROUND_DECIMAL)
@@ -63,6 +38,13 @@ print(TABLE_PATH)
 
 
 PHOSPHATE_LINK_MASS = 61.95577  # P(30.97389) + 2*O(2*15.99491) + H(1.00783)
+
+
+# This dictates a relative matching threshold such that we consider abs(sum(masses)/target_mass - 1) < MATCHING_THRESHOLD to be matched!
+MATCHING_THRESHOLD = 20e-6
+# We choose 20 ppm as the default error from the MS.
+# The error is on the higher side than would be for a good calibrated machine (6ppm),
+# but in the absence of an experimental measurement of this error, this (very) conservative value works well!
 
 
 # METHOD: Precompute all weight changes caused by breakages and adapt the
@@ -103,19 +85,42 @@ for start, end in list(
 BREAKAGES = {int(val / TOLERANCE): BREAKAGES[val] for val in BREAKAGES.keys()}
 
 
-# This dictates a relative matching threshold such that we consider abs(sum(masses)/target_mass - 1) < MATCHING_THRESHOLD to be matched!
-MATCHING_THRESHOLD = 20e-6
-# We choose 20 ppm as the default error from the MS.
-# The error is on the higher side than would be for a good calibrated machine (6ppm),
-# but in the absence of an experimental measurement of this error, this (very) conservative value works well!
+def initialize_nucleotide_df(reduce_table, reduce_set):
+    masses = pl.read_csv(
+        (
+            importlib.resources.files(__package__)
+            / "assets"
+            / f"{"masses_bases" if reduce_set else "masses"}.tsv"
+        ),
+        separator="\t",
+    )
+    # Note: "masses.tsv" has multiples nucleosides with the same mass!
 
-EXPLANATION_MASSES = UNIQUE_MASSES.with_columns(
-    ((pl.col("monoisotopic_mass") + PHOSPHATE_LINK_MASS) / TOLERANCE)
-    .round(0)
-    .cast(pl.Int64)
-    .alias("tolerated_integer_masses")
+    assert masses.columns == _COLS
+
+    masses = masses.with_columns(pl.col("monoisotopic_mass").round(ROUND_DECIMAL))
+
+    unique_masses = (
+        masses.group_by("monoisotopic_mass", maintain_order=True)
+        .first()
+        .select(pl.col(_COLS))
+    )
+    # For mass explanation for ladder building, to convert the masses to an integer value for the DP algorithm!
+
+    explanation_masses = unique_masses.with_columns(
+        ((pl.col("monoisotopic_mass") + PHOSPHATE_LINK_MASS) / TOLERANCE)
+        .round(0)
+        .cast(pl.Int64)
+        .alias("tolerated_integer_masses")
+    )
+
+    max_mass = explanation_masses.select(
+        pl.col("tolerated_integer_masses")
+    ).max().item() * (12 if reduce_table else 35)
+
+    return masses, unique_masses, explanation_masses, max_mass
+
+
+MASSES, UNIQUE_MASSES, EXPLANATION_MASSES, MAX_MASS = initialize_nucleotide_df(
+    REDUCE_TABLE, REDUCE_SET
 )
-
-MAX_MASS = EXPLANATION_MASSES.select(
-    pl.col("tolerated_integer_masses")
-).max().item() * (12 if REDUCE_TABLE else 35)
