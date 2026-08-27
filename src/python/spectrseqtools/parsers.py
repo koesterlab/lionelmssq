da0348dcf9aa84ce9838138a022a83193b705acb
Laura C. Kühle <laura.kuehle@hhu.de>
2026-08-27 13:02:52 +0200

    Add subparser to preprocess complex mixtures

--- src/python/spectrseqtools/parsers.py
+++ src/python/spectrseqtools/parsers.py
@@ -544,6 +544,154 @@ class PostprocessingOptions(ddargparse.OptionsBase):
 
 
 @dataclass
+class MixturePreprocessingOptions(ddargparse.OptionsBase):
+    """Preprocessing for complex mixtures."""
+
+    input: Path = field(
+        metadata={"help": "Path to input file in RAW format"},
+    )
+    meta: Path = field(
+        metadata={"help": "Path to YAML with meta information."},
+    )
+    output_dir: Path = field(
+        metadata={
+            "help": "Output directory.",
+        },
+    )
+    alphabet: Path | None = field(
+        default=None,
+        metadata={"help": "Path to file containing nucleotide alphabet."},
+    )
+    window_size: float = field(
+        default=0.2,
+        metadata={
+            "help": "Window size to consider for peak selection.",
+        },
+    )
+    min_time: float | None = field(
+        default=None,
+        metadata={
+            "help": "First time point to consider for peak selection (optional).",
+        },
+    )
+    max_time: float | None = field(
+        default=None,
+        metadata={
+            "help": "Last time point to consider for peak selection (optional).",
+        },
+    )
+    min_intensity: float | None = field(
+        default=None,
+        metadata={
+            "help": "Minimum intensity required for peak consideration "
+            "(used in ms_deisotope package as 'minimum_intensity')."
+        },
+    )
+    tolerance: float = field(
+        default=10e-6,
+        metadata={"help": "Error tolerance to consider masses identical."},
+    )
+    num_decimal_places: int = field(
+        default=3,
+        metadata={"help": "Number of considered decimal places for precision."},
+    )
+    boundary_factor: int = field(
+        default=2,
+        metadata={"help": "Factor for scaling theoretical singleton boundaries."},
+    )
+    min_precursor_charge: int = field(
+        default=3,
+        metadata={"help": "Minimum MS1 charge to consider associated MS2 scans."},
+    )
+    isotopic_shift_factor: int = field(
+        default=10,
+        metadata={"help": "Factor for scaling isotopic shift for precursors."},
+    )
+    intact_mass_cutoff_factor: float = field(
+        default=0.4,
+        metadata={
+            "help": "Factor for maximum mass to function as lower bound on intact mass."
+        },
+    )
+    envelope_min_score: float = field(
+        default=150.0,
+        metadata={
+            "help": "Minimum accepted score during envelope fitting "
+            "(used in ms_deisotope package as 'minimum_score')."
+        },
+    )
+    envelope_error_tol: float = field(
+        default=0.02,
+        metadata={
+            "help": "Error tolerance for envelopes during fitting "
+            "(used in ms_deisotope package as 'mass_error_tolerance')."
+        },
+    )
+    averagine_backbone: AveragineBackbone = field(
+        default=AveragineBackbone.PHOSPHATE,
+        metadata={
+            "help": "Backbone considered in Averagine model "
+            "(used in ms_deisotope package)."
+        },
+    )
+    max_missed_peaks: int = field(
+        default=0,
+        metadata={
+            "help": "Maximum number of missed peaks tolerated in envelope fitting "
+            "(used in ms_deisotope package)."
+        },
+    )
+    scale_method: str = field(
+        default="sum",
+        metadata={
+            "help": "Scale method for intensity values (used in ms_deisotope package)."
+        },
+    )
+    peak_error_tol: float = field(
+        default=2e-5,
+        metadata={
+            "help": "Error tolerance for each individual peak "
+            "(used in ms_deisotope package as 'error_tolerance')."
+        },
+    )
+    ms1_charge_range: tuple[int, int] | None = field(
+        default=None,
+        metadata={
+            "help": "Charge range considered for MS1 deconvolution "
+            "(used in ms_deisotope package)."
+        },
+    )
+    ms2_charge_range: tuple[int, int] | None = field(
+        default=None,
+        metadata={
+            "help": "Charge range considered for MS2 deconvolution "
+            "(used in ms_deisotope package)."
+        },
+    )
+    ms1_truncate_after: float = field(
+        default=0.95,
+        metadata={
+            "help": "Percentage of included isotopic patterns for MS1 scans "
+            "(used in ms_deisotope package)."
+        },
+    )
+    ms2_truncate_after: float = field(
+        default=0.9,
+        metadata={
+            "help": "Percentage of included isotopic patterns for MS2 scans "
+            "(used in ms_deisotope package)."
+        },
+    )
+
+
+@dataclass
+class MixtureOptions(ddargparse.OptionsBase):
+    """Analysis of complex mixtures."""
+
+    preprocessing: MixturePreprocessingOptions | None
+
+
+@dataclass
 class Options(ddargparse.OptionsBase):
     """
     De novo prediction of RNA sequences
@@ -561,3 +709,4 @@ class Options(ddargparse.OptionsBase):
     prediction: PredictionOptions | None
     postprocessing: PostprocessingOptions | None
     plotting: PlottingOptions | None
+    mixture: MixtureOptions | None
